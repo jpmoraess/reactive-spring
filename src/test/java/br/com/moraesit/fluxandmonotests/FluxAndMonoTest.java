0182c90443e90b5052cc573d23f62eef08bb80dc
jpmoraess <moraesdeveloperweb@gmail.com>
2021-08-15 01:00:00 -0300

    writing JUnit tests

--- src/test/java/br/com/moraesit/fluxandmonotests/FluxAndMonoTest.java
+++ src/test/java/br/com/moraesit/fluxandmonotests/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package br.com.moraesit.fluxandmonotests;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -15,5 +16,56 @@ public class FluxAndMonoTest {
         stringFlux.subscribe(System.out::println,
                 (ex) -> System.err.println("Exception is: " + ex),
                 () -> System.out.println("Completed with successfully.."));
+    }
+
+    @Test
+    public void fluxTestElementsWithoutError() {
+        Flux<String> stringFlux = Flux.just("João", "Pedro", "Moraes")
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("João")
+                .expectNext("Pedro")
+                .expectNext("Moraes")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxTestElementsWithError() {
+        Flux<String> stringFlux = Flux.just("João", "Pedro", "Moraes")
+                .concatWith(Flux.error(new RuntimeException("Exception....")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("João")
+                .expectNext("Pedro")
+                .expectNext("Moraes")
+                //.expectError(RuntimeException.class)
+                .expectErrorMessage("Exception....")
+                .verify();
+    }
+
+    @Test
+    public void fluxTestElementsCountWithError() {
+        Flux<String> stringFlux = Flux.just("João", "Pedro", "Moraes")
+                .concatWith(Flux.error(new RuntimeException("Exception....")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(3)
+                .expectErrorMessage("Exception....")
+                .verify();
+    }
+
+    @Test
+    public void fluxTestElementsWithError1() {
+        Flux<String> stringFlux = Flux.just("João", "Pedro", "Moraes")
+                .concatWith(Flux.error(new RuntimeException("Exception....")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("João", "Pedro", "Moraes")
+                .expectErrorMessage("Exception....")
+                .verify();
     }
 }
